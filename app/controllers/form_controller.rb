fb2261b33b48f6b90d80d562d9564753091c231e
Sampozzo <nicola@lanciano.it>
2023-01-03 01:48:12 +0100

    Bootstrap template. work in progress 2

--- app/controllers/form_controller.rb
+++ app/controllers/form_controller.rb
@@ -1,6 +1,7 @@
 class FormController < ApplicationController
   def index
-    @paste_active = "active"
+    @home_active = "active"
+    @home_tab_active = "show active"
     @preview_enabled = "disabled"
     @copy_enabled = "disabled"
 
@@ -37,44 +38,62 @@ class FormController < ApplicationController
       formatter = Rouge::Formatters::HTMLInline.new(@theme)
       lexer = Rouge::Lexer.find(@lang)
       #lexer = Rouge::Lexers::Shell.new
-      if @theme == 'base16.light'
-        @first_block = '<div style="overflow:auto;position: relative;margin-bottom: 1em;color: #303030;font-family: Monaco,Consolas,monospace;font-size: 1.0em;line-height: 1.8;border-radius:4px;"><pre>'
-      elsif @theme == 'base16.dark'
-        @first_block = '<div style="overflow:auto;position: relative;margin-bottom: 1em;background: #151515;color: #d0d0d0;font-family: Monaco,Consolas,monospace;font-size: 1.0em;line-height: 1.8;border-radius:4px;"><pre>'
-      elsif @theme == 'base16.monokai.light'
-        @first_block = '<div style="overflow:auto;position: relative;margin-bottom: 1em;color: #f8f8f2;font-family: Monaco,Consolas,monospace;font-size: 1.0em;line-height: 1.8;border-radius:4px;"><pre>'
+      if @theme == 'base16.dark'
+        @color = "#d0d0d0"
+        @background = "#151515"
+      elsif @theme == 'base16.light'
+        @color = "#303030"
+        @background = ""
       elsif @theme == 'base16.monokai.dark'
-        @first_block = '<div style="overflow:auto;position: relative;margin-bottom: 1em;background: #272822;color: #f8f8f2;font-family: Monaco,Consolas,monospace;font-size: 1.0em;line-height: 1.8;border-radius:4px;"><pre>'
-      elsif @theme == 'base16.solarized.light'
-        @first_block = '<div style="overflow:auto;position: relative;margin-bottom: 1em;color: #586e75;font-family: Monaco,Consolas,monospace;font-size: 1.0em;line-height: 1.8;border-radius:4px;"><pre>'
+        @color = "#f8f8f2"
+        @background = "#272822"
+      elsif @theme == 'base16.monokai.light'
+        @color = "#f8f8f2"
+        @background = ""
       elsif @theme == 'base16.solarized.dark'
-        @first_block = '<div style="overflow:auto;position: relative;margin-bottom: 1em;background: #002b36;color: #93a1a1;font-family: Monaco,Consolas,monospace;font-size: 1.0em;line-height: 1.8;border-radius:4px;"><pre>'
+        @color = "#93a1a1"
+        @background = "#002b36"
+      elsif @theme == 'base16.solarized.light'
+        @color = "#586e75"
+        @background = ""
       elsif @theme == 'colorful'
-        @first_block = '<div style="overflow:auto;position: relative;margin-bottom: 1em;background: #000;color: #bbbbbb;font-family: Monaco,Consolas,monospace;font-size: 1.0em;line-height: 1.8;border-radius:4px;"><pre>'
+        @color = "#bbbbbb"
+        @background = "#000"
       elsif @theme == 'github'
-        @first_block = '<div style="overflow:auto;position: relative;margin-bottom: 1em;background: #f8f8f8;color: #212529;font-family: Monaco,Consolas,monospace;font-size: 1.0em;line-height: 1.8;border-radius:4px;"><pre>'
+        @color = "#212529"
+        @background = "#f8f8f8"
       elsif @theme == 'gruvbox.light'
-        @first_block = '<div style="overflow:auto;position: relative;margin-bottom: 1em;background: #fbf1c7;color: #282828;font-family: Monaco,Consolas,monospace;font-size: 1.0em;line-height: 1.8;border-radius:4px;"><pre>'
+        @color = "#282828"
+        @background = "#fbf1c7"
       elsif @theme == 'gruvbox.dark'
-        @first_block = '<div style="overflow:auto;position: relative;margin-bottom: 1em;background: #282828;color: #fbf1c7;font-family: Monaco,Consolas,monospace;font-size: 1.0em;line-height: 1.8;border-radius:4px;"><pre>'
+        @color = "#fbf1c7"
+        @background = "#282828"
       elsif @theme == 'igorpro'
-        @first_block = '<div style="overflow:auto;position: relative;margin-bottom: 1em;color: #444444;font-family: Monaco,Consolas,monospace;font-size: 1.0em;line-height: 1.8;border-radius:4px;"><pre>'
+        @color = "#444444"
+        @background = ""
       elsif @theme == 'molokai'
-        @first_block = '<div style="overflow:auto;position: relative;margin-bottom: 1em;background: #1b1d1e;color: #f8f8f2;font-family: Monaco,Consolas,monospace;font-size: 1.0em;line-height: 1.8;border-radius:4px;"><pre>'
+        @color = "#f8f8f2"
+        @background = "#1b1d1e"
       elsif @theme == 'monokai'
-        @first_block = '<div style="overflow:auto;position: relative;margin-bottom: 1em;background: #49483e;color: #f8f8f2;font-family: Monaco,Consolas,monospace;font-size: 1.0em;line-height: 1.8;border-radius:4px;"><pre>'
+        @color = "#f8f8f2"
+        @background = "#49483e"
       elsif @theme == 'monokai.sublime'
-        @first_block = '<div style="overflow:auto;position: relative;margin-bottom: 1em;color: #ffffff;font-family: Monaco,Consolas,monospace;font-size: 1.0em;line-height: 1.8;border-radius:4px;"><pre>'
+        @color = "#ffffff"
+        @background = ""
       elsif @theme == 'pastie'
-        @first_block = '<div style="overflow:auto;position: relative;margin-bottom: 1em;color: #212529;font-family: Monaco,Consolas,monospace;font-size: 1.0em;line-height: 1.8;border-radius:4px;"><pre>'
+        @color = "#212529"
+        @background = ""
       elsif @theme == 'thankful_eyes'
-        @first_block = '<div style="overflow:auto;position: relative;margin-bottom: 1em;background: #122b3b;color: #faf6e4;font-family: Monaco,Consolas,monospace;font-size: 1.0em;line-height: 1.8;border-radius:4px;"><pre>'
+        @color = "#faf6e4"
+        @background = "#122b3b"
       elsif @theme == 'tulip'
-        @first_block = '<div style="overflow:auto;position: relative;margin-bottom: 1em;background: #231529;color: #FFFFFF;font-family: Monaco,Consolas,monospace;font-size: 1.0em;line-height: 1.8;border-radius:4px;"><pre>'
+        @color = "#FFFFFF"
+        @background = "#231529"
       end
+      @first_block = '<div style="overflow:auto;position: relative;margin-bottom: 1em;background: ' + @background + ';color: ' + @color + ';font-family: Monaco,Consolas,monospace;font-size: 1.0em;line-height: 1.8;border-radius:4px;"><pre>'
       @second_block = formatter.format(lexer.lex(@source))
       @final_block = '</pre></div>'
-      #File.delete("tmp/uploads/" + @file_name) if File.exist?("tmp/uploads/" + @file_name)
+      File.delete("tmp/uploads/" + @file_name) if File.exist?("tmp/uploads/" + @file_name)
     else 
       redirect_to controller: 'form', action: "index"
     end
